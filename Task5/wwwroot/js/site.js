3453f4c466aa396bb1d6540b3b1b0c66ef234a2d
Salt-Upon-Wounds <kiw197204@gmail.com>
2023-06-06 02:45:22 +0300

    заготовка

--- Task5/wwwroot/js/site.js
+++ Task5/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const regionSelect = document.getElementById('select');
+const errorsInput = document.getElementById('errorsAmount');
+const seedInput = document.getElementById('seed');
+const seedButton = document.getElementById('seedRand');
+const csvButton = document.getElementById('csv');
+const table = document.getElementById('table').getElementsByTagName('tbody')[0];
