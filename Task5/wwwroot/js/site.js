4da32c43a8bc91596c58eb7b00cee470ed1fe877
Salt-Upon-Wounds <kiw197204@gmail.com>
2023-06-06 12:54:14 +0300

    рандомные данные без ошибок и csv

--- Task5/wwwroot/js/site.js
+++ Task5/wwwroot/js/site.js
@@ -11,19 +11,24 @@ const seedButton = document.getElementById('seedRand');
 const csvButton = document.getElementById('csv');
 const table = document.getElementById('table').getElementsByTagName('tbody')[0];
 
+let mistakes = 0;
 let faker = fakerRU;
 
 let data = [];
+let page = 0;
+let seed = 0;
 
 const restart = () => {
     document.getElementsByTagName("tbody")[0].innerHTML = "";
     data = [];
+    page = 0;
     getData();
 }
 
 const getData = () => {
-    console.log(faker);
-    for (let i = 0; i < 10; i++) {
+    //добавить ошибки и сохранение данных в массив для формирования csv
+    faker.seed(seed + page);
+    for (let i = 0; i < 20; i++) {
         let row = table.insertRow(table.rows.length - 1);
         row.insertCell().append(document.createTextNode(data.length))
         row.insertCell().appendChild(document.createTextNode(faker.string.uuid()));
@@ -31,8 +36,62 @@ const getData = () => {
         row.insertCell().appendChild(document.createTextNode(faker.location.streetAddress()));
         row.insertCell().appendChild(document.createTextNode(faker.phone.number()));
     }
+    page++;
 }
 
 window.addEventListener('load', () => {
     getData();
 });
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) {
+        return;
+    } else {
+        getData();
+    }
+});
+
+errorsInput.oninput = (event) => {
+    if (Number.isInteger(Number(event.target.value))) {
+        if (event.target.value === '') {
+            mistakes = 0;
+            restart();
+        } else if (event.target.value >= 0) {
+            mistakes = Number(event.target.value);
+            restart();
+        }
+    } 
+}
+
+seedInput.oninput = (event) => {
+    if (Number.isInteger(Number(event.target.value))) {
+        if (event.target.value === '') {
+            seed = 0;
+            restart();
+        } else if (event.target.value >= 0) {
+            seed = Number(event.target.value);
+            restart();
+        }
+    }
+}
+
+seedButton.onclick = () => {
+    let r = Math.floor(Math.random() * 999999);
+    seedInput.value = r;
+    seed = r;
+    restart();
+}
+
+csvButton.onclick = () => {
+    //допиши
+}
+
+regionSelect.onchange = (event) => {
+    switch (event.target.value) {
+        case '0': faker = fakerRU; break;
+        case '1': faker = fakerDE; break;
+        case '2': faker = fakerPL; break;
+        default: faker = fakerRU; break;
+    }
+    restart();
+}
